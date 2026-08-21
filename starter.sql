28c2af846d424dec00c38e0d499d39c5705a5c46
CHARUKESNI-1U25CS016 <charu231107@gmail.com>
2026-08-21 11:23:41 +0530

    Initialize collegeDB and create department table

--- starter.sql
+++ starter.sql
@@ -1,1 +1,5 @@
-
+create database collegeDB;
+use collegeDB;
+create table department(departmentID integer(5) primarykey,department varchar(20),HOD varchar(20));
+desc department;
+select*from department;
